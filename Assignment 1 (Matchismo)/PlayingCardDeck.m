25e2b05dcb00bee36a1f9ffdec59fcdec03f64d2
Nantook <brettnish10@gmail.com>
2015-02-19 12:00:08 -0700

    fixed filepath

---
+++ Assignment 1 (Matchismo)/PlayingCardDeck.m
@@ -0,0 +1,35 @@
+//
+//  PlayingCardDeck.m
+//  Stanford CS 193P Assignments
+//
+//  Created by Brett Nishikawa on 2015-02-17.
+//
+//
+
+#import "PlayingCardDeck.h"
+#import "PlayingCard.h"
+
+@implementation PlayingCardDeck
+
+- (instancetype)init
+{
+    self = [super init];
+    
+    if (self)
+    {
+        for (NSString *suit in [PlayingCard validSuits])
+        {
+            for (NSUInteger rank = 1; rank <= [PlayingCard maxRank]; rank++)
+            {
+                PlayingCard *card = [[PlayingCard alloc] init];
+                card.rank = rank;
+                card.suit = suit;
+                [self addCard:card];
+            }
+        }
+    }
+    
+    return self;
+}
+
+@end
